5289b9ebeabc2f4a674b958d883b81c675f6a831
Ramon Fávero <ramon.favero@hotmail.com>
2023-02-27 17:41:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -293,6 +293,4 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, "0.0.0.0", function() {
-  console.log("Server has started on port "+port);
-});
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
